b3642a826be88e1320bfd76f6eae16f374b57215
Siddhartha <siddureddyvr1115@email.com>
2025-10-26 11:21:37 +0530

    Your comment

--- WordGame.java
+++ WordGame.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class WordGame {
+public class WordGame { //Use Hashmap to solve frequency related prblms
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
