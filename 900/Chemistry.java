5c41cde43f8f22266698207842aa33172f42916d
Siddhartha <siddureddyvr1115@email.com>
2026-02-02 08:42:10 +0530

    900

---
+++ 900/Chemistry.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Chemistry {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            String s = sc.next();
+
+            HashMap<Character,Integer> map = new HashMap<>();
+
+
+            for(char c: s.toCharArray()){
+               map.put(c, map.getOrDefault(c, 0)+1);
+            }
+
+            int odd_freq = 0;
+
+            for(int count: map.values()){
+                if(count%2!=0){
+                    odd_freq++;
+                }
+            }
+
+            if(odd_freq > k+1){
+                    System.out.println("NO");
+            }
+
+            else{
+                    System.out.println("YES");
+            }
+        }
+    }
+}
---
+++ 900/Forked.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Forked {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int xk = sc.nextInt();
+            int yk = sc.nextInt();
+
+            int xq = sc.nextInt();
+            int yq = sc.nextInt();
+
+            int dX[] = {a,b,a,b,-a,-a,-b,-b};
+            int dY[] = {b,a,-b,-a,b,-b,a,-a};
+
+            HashSet<String> kingAttackerPos = new HashSet<>();
+            HashSet<String> queenAttackerPos = new HashSet<>();
+
+            for(int i=0; i<8; i++){
+                kingAttackerPos.add((xk+dX[i]) + "," + (yk+dY[i]));
+                queenAttackerPos.add((xq+dX[i]) + "," + (yq+dY[i]));
+            }
+
+            int count = 0;
+            for(String pos: kingAttackerPos){
+                if(queenAttackerPos.contains(pos)){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
