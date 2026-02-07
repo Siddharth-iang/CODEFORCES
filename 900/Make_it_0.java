73ede4ff2841bad3efb8c17c0489aa0076d29f6b
Siddharth-iang <siddureddyvr1115@gmail.com>
2026-02-08 05:20:19 +0530

    Create Make_it_0.java

---
+++ 900/Make_it_0.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Make_it_0 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            long a[] = new long[n];
+            for(int i=0; i<n; i++){
+                a[i] = Long.parseLong(st.nextToken());
+            }
+
+            if(n%2==0){
+                System.out.println(2);
+                System.out.println(1 +  " " + n);
+                System.out.println(1 + " " + n);
+            }
+
+            else{
+                System.out.println(4);
+                System.out.println(1 +  " " + (n-1));
+                System.out.println(1 + " " + (n-1));
+                System.out.println((n-1) + " " + n);
+                System.out.println((n-1) + " " + n);
+            }
+        }
+    }
+}
